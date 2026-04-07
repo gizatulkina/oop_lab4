70e8352e11f33f9931e5f012e435d94d2b5a799e
gizatulkina <yulia.giza7ullina@yandex.ru>
2026-04-07 13:41:39 +0500

    Создала базовое PyQt6 приложение с главным окном

---
+++ lab4.py
@@ -0,0 +1,23 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget
+
+
+class Canvas(QWidget):
+    def __init__(self):
+        super().__init__()
+
+
+class Editor(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Визуальный редактор")
+        self.resize(800, 600)
+
+        self.canvas = Canvas()
+        self.setCentralWidget(self.canvas)
+
+
+app = QApplication(sys.argv)
+window = Editor()
+window.show()
+sys.exit(app.exec())
